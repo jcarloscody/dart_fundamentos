1ac3aa3f5b6667523e1dcb35ac67b1d1e97016a2
Josué Carlos <josuecarlosos2@gmail.com>
2022-06-23 11:29:35 -0300

    ..

---
+++ lib/modificadores/mod.dart
@@ -0,0 +1,11 @@
+void main() {
+  String? n;
+
+  final nomeCompletoFinal;
+  nomeCompletoFinal = n; //
+
+  const nomeConst =
+      n; //a variavel constante já deve conhecer seus valores deste a compilação. a variavel "n" só será conhecida na execução
+
+  //final:
+}
