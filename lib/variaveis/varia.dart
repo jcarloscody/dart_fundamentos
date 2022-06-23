f7a2f337c1ab251f9defa03800b94957bdb2b1b0
Josué Carlos <josuecarlosos2@gmail.com>
2022-06-23 08:37:07 -0300

    variaveis

---
+++ lib/variaveis/varia.dart
@@ -0,0 +1,13 @@
+void main() {
+  int age = 13;
+  double price = 12.2;
+  String names = "lk";
+
+  String camelCase = "lk";
+
+  var ageImplicito = 12;
+
+  Object objetoQq;
+
+  dynamic dinamico;
+}
