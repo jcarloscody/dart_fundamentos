8a4b47fe368acc84043432c34198b07798e01610
Josué Carlos <josuecarlosos2@gmail.com>
2022-06-23 08:54:20 -0300

    null safety

--- lib/variaveis/nullsafety.dart
+++ lib/variaveis/nullsafety.dart
@@ -1,1 +1,25 @@
-void main() {}
+String
+    variavelNivelSuperior; //erro, este tipo de variável deve ser inicializado
+String? variavelNivelSuperior2; //
+
+void main() {
+  // String nome = null; //erro
+  String? nomeComNulo = null;
+
+  String? city; //variáveis locais não precisam ser inicializadas imediatamente
+
+  variavelNivelSuperior2 = '';
+  print(variavelNivelSuperior2
+      .length); //o dart não promove variavel de nivel superior de nula para não nula
+
+  String? n = null;
+  n = ''; //variavel local nula é promovida a não nula
+  print(n.length);
+
+  if (nomeComNulo != null) {
+    //checagem de nulo
+    print(nomeComNulo.length);
+  }
+
+  print(nomeComNulo!.length); //dev garantindo que não é nulo
+}
